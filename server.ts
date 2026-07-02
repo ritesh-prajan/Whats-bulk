7afd181cc124a2f5f01fd88e169f21bb0bc7897d
Ritesh Prajan . S <riteshprajanwork@gmail.com>
2026-07-03 00:05:11 +0530

    fixed_resource_resolution

--- server.ts
+++ server.ts
@@ -840,7 +840,7 @@ async function startServer() {
         }
       });
     } else {
-      const distPath = path.join(process.cwd(), 'dist');
+      const distPath = resolvedDirname;
       app.use(express.static(distPath));
       app.get('*', (req, res) => {
         res.sendFile(path.join(distPath, 'index.html'));
